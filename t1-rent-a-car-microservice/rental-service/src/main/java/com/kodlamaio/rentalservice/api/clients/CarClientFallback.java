378d50e9c695fe97befe057b66c23443e77b8cc8
Melisa Demirhan <melisa.dmrhn2202@gmail.com>
2023-05-20 01:46:50 +0300

    fallback retry added

--- t1-rent-a-car-microservice/rental-service/src/main/java/com/kodlamaio/rentalservice/api/clients/CarClientFallback.java
+++ t1-rent-a-car-microservice/rental-service/src/main/java/com/kodlamaio/rentalservice/api/clients/CarClientFallback.java
@@ -1,6 +1,8 @@
 package com.kodlamaio.rentalservice.api.clients;
 
 import com.kodlamaio.commonpackage.utils.dto.ClientResponse;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.retry.annotation.Retry;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
